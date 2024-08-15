48c6afce736d3de00b548511fb97904c8acc5dbe
habib-153 <h.r.sihab155@gmail.com>
2024-08-15 14:02:08 +0600

    static port giveb

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import mongoose from 'mongoose';
 import { Server } from 'http';
 
 let server: Server;
+const port = 5000
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`Example app listening on port ${port}`);
     });
   } catch (err) {
     console.log(err);

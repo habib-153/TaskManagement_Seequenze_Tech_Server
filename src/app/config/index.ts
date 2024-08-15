3462c4bcc716a4718f66b40f50f8d32399289802
habib-153 <h.r.sihab155@gmail.com>
2024-08-15 13:35:35 +0600

    config edited

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,9 +6,6 @@ dotenv.config({ path: path.join((process.cwd(), '.env')) });
 export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
-  jwt_secret: process.env.ACCESS_TOKEN_SECRET,
-  jwt_expires_in: process.env.JWT_EXPIRE,
-  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   NODE_ENV: process.env.NODE_ENV,
   twitch_client_id: process.env.TWITCH_CLIENT_ID,
   twitch_access_token: process.env.TWITCH_ACCESS_TOKEN,
